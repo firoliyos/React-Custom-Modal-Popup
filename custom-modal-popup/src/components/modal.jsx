5de47d6c96175ab6b307b91ff3e2ff4046a54ae4
firoliyos <firatesfaye44@gmail.com>
2024-10-29 15:04:45 +0300

    modal.jsx-functionality-done

--- custom-modal-popup/src/components/modal.jsx
+++ custom-modal-popup/src/components/modal.jsx
@@ -1,9 +1,26 @@
-
-
-
-
-export default function Modal() {
-  return(
-    <div>MODAL</div>
-  )
+export default function Modal({id,header,body,footer}) {
+  return (
+    <div id={id || "Modal"} className="modal">
+      <div className="modal-content">
+        <div className="header">
+           <span className="close-modal-icon">&times;</span> 
+           <h2>{header ? header : 'Header'}</h2>
+        </div>
+        <div className="body">
+          {
+            body ? body : <div>
+                <p>This is our modal Body</p>
+            </div>
+          }   
+        </div>
+        <div className="footer">
+            {
+                footer ? footer : <div>
+                    <h2>This is our Footer</h2>
+                </div>
+            }
+        </div>
+      </div>
+    </div>
+  );
 }
