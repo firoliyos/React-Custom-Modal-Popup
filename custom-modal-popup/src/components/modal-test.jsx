e2eb45c91a2c969acd3cfedb7d4d26ed4c96ed93
firoliyos <firatesfaye44@gmail.com>
2024-10-29 14:38:38 +0300

    modal-test-works

---
+++ custom-modal-popup/src/components/modal-test.jsx
@@ -0,0 +1,21 @@
+import {useState} from 'react'
+import Modal from "./modal"
+
+
+
+
+export default function ModalTest() {
+
+    const [showModalPopup, setShowModalPopup] = useState(false)
+    function handlePopup() {
+        setShowModalPopup(!showModalPopup)
+    }
+   return(
+    <div>
+      <button onClick={handlePopup} >Open Modal Popup</button> 
+      {
+         showModalPopup && <Modal />
+      } 
+    </div>
+   )
+}
